bd3192c4f866d415c8ade6db0f5a743ec7b64cee
Sean Shubin <sshubin@cj.com>
2018-09-11 17:28:27 -0700

    Configured supervisor

--- src/main/kotlin/com/seanshubin/devops/GlobalConstants.kt
+++ src/main/kotlin/com/seanshubin/devops/GlobalConstants.kt
@@ -15,4 +15,44 @@ object GlobalConstants {
     const val DatomicVersion = "0.9.5703"
     const val SecurityGroup = "ssh-8080"
     const val SchulzeJar = "$UserHomeDirName/github/sean/schulze/server/target/schulze.jar"
+    const val SchulzeDatabase = "$UserHomeDirName/backup/datomic-schulze/datomic.h2.db"
+
+    val SchulzeSupervisorConfig = """
+        [program:schulze]
+        directory=/home/ec2-user/schulze/
+        command=java -Djava.io.tmpdir=/tmp -cp schulze.jar com.seanshubin.schulze.server.ServerApplication 8080 datomic:free://localhost:4334/schulze
+        stdout_logfile=/home/ec2-user/schulze/log/out.log
+        stderr_logfile=/home/ec2-user/schulze/log/err.log
+        autostart=true
+    """.trimIndent()
+
+    val DatomicSupervisorConfig = """
+        [program:datomic]
+        directory=/home/ec2-user/datomic/
+        command=/home/ec2-user/datomic/bin/transactor /home/ec2-user/datomic/config/samples/free-transactor-template.properties
+        stdout_logfile=/home/ec2-user/datomic/log/out.log
+        stderr_logfile=/home/ec2-user/datomic/log/err.log
+        autostart=true
+    """.trimIndent()
+
+    val SupervisorConfig = """
+        [unix_http_server]
+        file=/tmp/supervisor.sock   ; the path to the socket file
+
+        [supervisord]
+        logfile=/tmp/supervisord.log ; main log file; default ${'$'}CWD/supervisord.log
+        logfile_maxbytes=50MB        ; max main logfile bytes b4 rotation; default 50MB
+        logfile_backups=10           ; # of main logfile backups; 0 means none, default 10
+        loglevel=info                ; log level; default info; others: debug,warn,trace
+        pidfile=/tmp/supervisord.pid ; supervisord pidfile; default supervisord.pid
+        nodaemon=false               ; start in foreground if true; default false
+        minfds=1024                  ; min. avail startup file descriptors; default 1024
+        minprocs=200                 ; min. avail process descriptors;default 200
+
+        [rpcinterface:supervisor]
+        supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+        [supervisorctl]
+        serverurl=unix:///tmp/supervisor.sock ; use a unix:// URL  for a unix socket
+    """.trimIndent()
 }
--- src/main/kotlin/com/seanshubin/devops/Prototype.kt
+++ src/main/kotlin/com/seanshubin/devops/Prototype.kt
@@ -1,42 +1,127 @@
 package com.seanshubin.devops
 
-fun datomicServer() {
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun trackTimeMilliseconds(block: () -> Unit): Long {
+    val startTime = System.currentTimeMillis()
+    block()
+    val endTime = System.currentTimeMillis()
+    val duration = endTime - startTime
+    return duration
+}
+
+class Prototype : Runnable {
+    val ec2 = DatomicServerDependencyInjection.ec2
+    val datomicVersion = GlobalConstants.DatomicVersion
+    val datomicDir = DatomicServerDependencyInjection.datomicDir
+    val schulzeDatabase = GlobalConstants.SchulzeDatabase
+    val createShellFromHost = DatomicServerDependencyInjection.createShellFromHost
+    val stabilizer = DatomicServerDependencyInjection.stabilizer
+    val localShell = DatomicServerDependencyInjection.localShell
     val schulzeJar = GlobalConstants.SchulzeJar
     val userName = GlobalConstants.UserName
     val privateKeyPathName = GlobalConstants.PrivateKeyPathName
-    val datomicVersion = GlobalConstants.DatomicVersion
-    val datomicDir = DatomicServerDependencyInjection.datomicDir
-    val ec2 = DatomicServerDependencyInjection.ec2
-    val log = DatomicServerDependencyInjection.logger
-    val localShell = DatomicServerDependencyInjection.localShell
-    ec2.listInstancesNotTerminated()
-    val createShellFromHost = DatomicServerDependencyInjection.createShellFromHost
-    val instanceId = ec2.createInstance(GlobalConstants.AmazonLinux2AMI, GlobalConstants.KeyName)
     val emit = DatomicServerDependencyInjection.emit
-    emit("instanceId = $instanceId")
-    val stabilizer = DatomicServerDependencyInjection.stabilizer
-    ec2.waitUntilReady(instanceId)
-    val host = ec2.getHost(instanceId)
-    emit("host = $host")
-    emit("ssh -i \"$privateKeyPathName\" $userName@$host")
-    val createShell: () -> SshShell = {
-        val shell = createShellFromHost(host)
-        shell.execute("pwd")
-        shell
-    }
-    val ssh = stabilizer.create("create shell", createShell)
-    ssh.execute("wget https://my.datomic.com/downloads/free/$datomicVersion -O $datomicDir.zip")
-    ssh.execute("unzip $datomicDir.zip")
 
-    stabilizer.waitUntilEqual("copying schulze app", 0){
-        localShell.executeAndReturnExitCode("scp", "-o", "StrictHostKeyChecking=no", "-i", privateKeyPathName, schulzeJar, "$userName@$host:")
+    var instanceId: String? = null
+    var ssh: SshShell? = null
+    var host: String? = null
+
+    override fun run() {
+        val time = trackTimeMilliseconds {
+            setupEc2()
+            setupDatomic()
+            setupSchulze()
+            setupJava()
+            setupSupervisor()
+        }
+        done(time)
     }
 
-    ssh.execute("sudo yum -y install java")
-//    ssh.execute("cd $datomicDir && bin/transactor config/samples/free-transactor-template.properties &")
-//    ssh.execute("java -jar schulze.jar 8080 datomic:free://localhost:4334/schulze &")
+    fun setupEc2() {
+        instanceId = ec2.createInstance(GlobalConstants.AmazonLinux2AMI, GlobalConstants.KeyName)
+        emit("instanceId = $instanceId")
+        ec2.waitUntilReady(instanceId!!)
+        host = ec2.getHost(instanceId!!)
+        emit("host = $host")
+        emit("ssh -i \"$privateKeyPathName\" $userName@$host")
+        val createShell: () -> SshShell = {
+            val shell = createShellFromHost(host!!)
+            shell.execute("pwd")
+            shell
+        }
+        ssh = stabilizer.create("create shell", createShell)
+    }
+
+    fun setupDatomic() {
+        sshExec("wget https://my.datomic.com/downloads/free/$datomicVersion -O $datomicDir.zip")
+        sshExec("unzip $datomicDir.zip")
+        sshExec("rm $datomicDir.zip")
+        sshExec("ln -s $datomicDir datomic")
+        sshExec("mkdir -p datomic/data/db")
+        sshExec("mkdir -p datomic/log")
+        secureCopy("schulze database", schulzeDatabase, "datomic/data/db/")
+    }
+
+    fun setupSchulze() {
+        sshExec("mkdir schulze")
+        secureCopy("schulze app", schulzeJar, "schulze/")
+        sshExec("mkdir -p schulze/log")
+    }
+
+    fun setupJava() {
+        sshExec("sudo yum -y install java")
+    }
+
+    fun setupSupervisor() {
+        sshExec("sudo easy_install supervisor")
+        val text = listOf(
+                GlobalConstants.SupervisorConfig,
+                GlobalConstants.DatomicSupervisorConfig,
+                GlobalConstants.SchulzeSupervisorConfig).joinToString("\n\n")
+        val tempDir = Paths.get("out", "temp")
+        Files.createDirectories(tempDir)
+        val supervisorConfigPath = tempDir.resolve("supervisord.conf")
+        Files.write(supervisorConfigPath, text.toByteArray(StandardCharsets.UTF_8))
+        secureCopy("supervisor config", supervisorConfigPath.toString(), "")
+        sshExec("sudo mv supervisord.conf /etc/")
+        sshExec("supervisord")
+    }
+
+    fun sshExec(command: String) {
+        ssh!!.execute(command)
+    }
+
+    fun secureCopy(caption: String, from: String, to: String) {
+        stabilizer.waitUntilEqual("copying $caption", 0) {
+            localShell.executeAndReturnExitCode("scp", "-o", "StrictHostKeyChecking=no", "-i", privateKeyPathName, from, "$userName@$host:$to")
+        }
+    }
+
+    fun done(timeInMilliseconds: Long) {
+        val lines = listOf(
+                "All done!",
+                "",
+                "$timeInMilliseconds milliseconds",
+                "Navigate here to the schulze app:",
+                "    $host:8080",
+                "To debug the application, you will need the private key 'schulze-voter.pem'",
+                "log in with:",
+                "    ssh -i \"schulze-voter.pem\" $userName@$host",
+                "tail the logs with:",
+                "    tail -f schulze/log/out.log schulze/log/err.log datomic/log/out.log datomic/log/err.log",
+                "check the status with:",
+                "    supervisorctl status",
+                "see how everything is configured by looking with:",
+                "    cat /etc/supervisord.conf",
+                "",
+                "$host:8080")
+        lines.forEach(emit)
+    }
 }
 
 fun main(args: Array<String>) {
-    datomicServer()
+    Prototype().run()
 }
